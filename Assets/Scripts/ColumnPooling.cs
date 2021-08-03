caa6a4d56466d6593b3982ec43beb62b4bfd7650
djennedy <darren_jennedy@sfu,ca>
2021-08-03 06:11:03 -0700

    Fixed column spawn rate and added buildings

--- Assets/Scripts/ColumnPooling.cs
+++ Assets/Scripts/ColumnPooling.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 
 public class ColumnPooling : MonoBehaviour
 {
-    public GameObject columnPrefab;                                    
+    public GameObject columnPrefab;
+    public GameObject constructionPrefab;
     public int columnPoolSize = 5;                                    
-    public float spawnRate = 4f;                                   
-    public float columnMin = -1f;                                    //Minimum y value of the column position.
-    public float columnMax = 3.5f;                                    //Maximum y value of the column position.
+    public float spawnRate = 10f;                                   
+    public float columnMin = -2f;                                    //Minimum y value of the column position.
+    public float columnMax = 2f;                                    //Maximum y value of the column position.
 
     private GameObject[] columns;                                    //Collection of pooled columns.
+    private GameObject[] buildings;
     private int currentColumn = 0;                                    //Index of the current column in the collection.
 
     private Vector2 poolPosition = new Vector2 (-2,-40);        //A holding position for our unused columns offscreen.
-    private float spawnXPosition = 10;
+    private float spawnXPosition = 10f;
 
-    private float timeSinceSpawned;
+    private float buildingXPositionOffset = 10f;
+    private float buildingYPosition = -1.25f;
+
+    private float timeSinceSpawned = 0;
 
     void Start()
     {
         columns = new GameObject[columnPoolSize];
+        buildings = new GameObject[columnPoolSize];
+
         for(int i = 0; i < columnPoolSize;i++){
-            columns[i] = (GameObject) Instantiate(columnPrefab, poolPosition, Quaternion.identity); 
+            columns[i] = (GameObject) Instantiate(columnPrefab, poolPosition, Quaternion.identity);
+            buildings[i] = (GameObject) Instantiate(constructionPrefab, poolPosition, Quaternion.identity);
         }
     }
 
@@ -41,6 +49,7 @@ public class ColumnPooling : MonoBehaviour
 
             //...then set the current column to that position.
             columns[currentColumn].transform.position = new Vector2(spawnXPosition, spawnYPosition);
+            buildings[currentColumn].transform.position = new Vector2(spawnXPosition + buildingXPositionOffset, buildingYPosition);
 
             //Increase the value of currentColumn. If the new size is too big, set it back to zero
             currentColumn ++;
